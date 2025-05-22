57b1e4f3c2071251bbd8ff7b823e9d0cb849f517
JO00o0 <yossefhassan788@gmail.com>
2025-05-22 18:56:01 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -50,5 +50,6 @@ def upload_image():
 
 # تشغيل السيرفر
 if __name__ == '__main__':
-    print("Starting Flask app...")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+           port =
+int(os.environ.get('PORT',5000))
+    app.run(host="0.0.0.0", port=port)
